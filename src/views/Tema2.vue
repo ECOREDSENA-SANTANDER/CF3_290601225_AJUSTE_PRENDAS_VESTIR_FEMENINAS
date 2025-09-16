2d23564fdf041dbbae7b03bf4569bd86adea3f80
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-09-16 16:19:42 -0500

    avance

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -103,39 +103,39 @@
 
     p.mb-4 Para iniciar el descosido de prendas con tejidos planos se ubica el desbaratador de modo que vaya rompiendo toda la línea de costura, debe finalizar retirando el exceso y si es necesario con la ayuda del corta hilos.
 
-    .row.justify-content-center.mb-4(data-aos="fade-left")
-      .col-lg-3
-        img.d-none.d-lg-block(src='@/assets/curso/Tema2/img02.png', alt='')
-      .col-lg-9
-        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
-          .row.align-items-center.justify-content-center(titulo="Desbaratar costura plana")
-            .col-md-4.mb-4.mb-md-0
-              p NO HAY INFORMACIÓN EN EL XD
-            .col-md-5.mb-4.mb-md-0
+    .row.mb-4
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.bg6.p-4
+          .row.justify-content-center.mb-3
+            h3.text-center.mb-4 Desbaratar costura plana
+            .col-lg-11
               .titulo-sexto.color-acento-contenido(data-aos='fade-right')
                 h5 Figura 5.  
                 span Paso 1
               img(src='@/assets/curso/Tema2/fig5.png', alt='Manos sostienen una pieza textil mientras una herramienta con punta curva es utilizada para deshacer una costura recta en el borde de la tela. Dedos sostienen dos capas de tela mientras se separan con precisión los puntos de una costura, en un proceso manual de descosido. Una mano levanta ligeramente una prenda mientras una herramienta de descosedor reposa sobre la superficie, rodeada de restos de hilo suelto.')
               figcaption Fuente: https://skarlett.es/como-descoser/ 
-          .row.align-items-center.justify-content-center(titulo="Cortahilos o desbaratador")
-            .col-md-4.mb-4.mb-md-0
-              p Si el textil es resistente puede probarse abriendo los textiles e ir cortando con el cortahilos o desbaratador.
-            .col-md-5.mb-4.mb-md-0
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0 
+        .tarjeta--boton.bg7.p-4
+          .row.justify-content-center.mb-3
+            p.text-center.text-small Si el textil es resistente puede probarse abriendo los textiles e ir cortando con el cortahilos o desbaratador.
+            .col-lg-11
               .titulo-sexto.color-acento-contenido(data-aos='fade-right')
                 h5 Figura 6.  
                 span Paso 2
-              img(src='@/assets/curso/Tema2/fig6.png', alt='Manos sostienen una pieza textil mientras una herramienta con punta curva es utilizada para deshacer una costura recta en el borde de la tela. Dedos sostienen dos capas de tela mientras se separan con precisión los puntos de una costura, en un proceso manual de descosido. Una mano levanta ligeramente una prenda mientras una herramienta de descosedor reposa sobre la superficie, rodeada de restos de hilo suelto.')
+              img(src='@/assets/curso/Tema2/fig6.png', alt='Dedos sostienen dos capas de tela mientras se separan con precisión los puntos de una costura, en un proceso manual de descosido.')
               figcaption Fuente: https://skarlett.es/como-descoser/ 
-          
-          .row.align-items-center.justify-content-center(titulo="Diseño conceptual")
-            .col-md-4.mb-4.mb-md-0
-              p NO HAY INFORMACIÓN EN EL XD
-            .col-md-5.mb-4.mb-md-0
+      
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0  
+        .tarjeta--boton.bg8.p-4
+          .row.justify-content-center.mb-3
+            h3.text-center.mb-4 Separación de las piezas
+            .col-lg-11
               .titulo-sexto.color-acento-contenido(data-aos='fade-right')
                 h5 Figura 7.  
                 span Paso 3
-              img(src='@/assets/curso/Tema2/fig7.png', alt='Manos sostienen una pieza textil mientras una herramienta con punta curva es utilizada para deshacer una costura recta en el borde de la tela. Dedos sostienen dos capas de tela mientras se separan con precisión los puntos de una costura, en un proceso manual de descosido. Una mano levanta ligeramente una prenda mientras una herramienta de descosedor reposa sobre la superficie, rodeada de restos de hilo suelto.')
+              img(src='@/assets/curso/Tema2/fig7.png', alt='Una mano levanta ligeramente una prenda mientras una herramienta de descosedor reposa sobre la superficie, rodeada de restos de hilo suelto.')
               figcaption Fuente: https://skarlett.es/como-descoser/ 
+         
           
     .row.justify-content-center.align-items-center.mb-5.bg5
       .col-lg-11.p-4
@@ -147,7 +147,7 @@
 
     .row.justify-content-center.align-items-center.mb-5
       .col-lg-10
-        PasosA.color-primario.mb-5(tipo="n")
+        PasosA.color-adicional-1.mb-5(tipo="n")
           .row
             .col-md-6.mb-4.mb-md-0
               h4 Paso 1. 
@@ -159,44 +159,68 @@
               img(src='@/assets/curso/Tema2/fig8.png', alt='Mano sostiene el borde de una tela con costura rematada mientras una herramienta puntiaguda es usada para retirar el hilo inferior, identificado como “áncora inferior”.')
               figcaption.text-regular Fuente: https://www.naiicostura.com/3-maneras-de-descoser-costura-overlock/
 
-          .row
-            .col-md-6.mb-4.mb-md-0
-              figure
-                img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-                figcaption Leyenda de la Imagen
-            .col-md-6
-              h3 Nullam vulputate mauris eros, ut.
-              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.          
+          .row.texto-izquierda 
+            .col-md-5
+              .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+                h5 Figura 9.  
+                span Paso 2
+              img(src='@/assets/curso/Tema2/fig9.png', alt='Dedos sujetan una tela con bordes rematados, parcialmente descosidos, mientras en el fondo se distingue una herramienta manual colocada sobre una superficie cuadriculada.')
+              figcaption.text-regular Fuente: Fuente: https://www.naiicostura.com/3-maneras-de-descoser-costura-overlock/
 
-          .row
             .col-md-6.mb-4.mb-md-0
-              h3 Duis laoreet est non ligula.
-              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.      
-            .col-md-6
-              figure
-                img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-                figcaption Leyenda de la imagen
+              h4 Paso 2. 
+              h4 Empezar a sacar los hilos cortados.
 
           .row
             .col-md-6.mb-4.mb-md-0
-              h3 Duis laoreet est non ligula.
-              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.      
-            .col-md-6
-              figure
-                img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-                figcaption Leyenda de la imagen
+              h4 Paso 3.  
+              h4 Costura deshecha.
+            .col-md-5
+              .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+                h5 Figura 10.  
+                span Paso 3 
+              img(src='@/assets/curso/Tema2/fig10.png', alt='Mano sostiene una pieza de tela con costura lateral deshecha, mientras varios fragmentos de hilo suelto reposan al lado sobre una superficie cuadriculada.')
+              figcaption.text-regular Fuente: Fuente: https://www.naiicostura.com/3-maneras-de-descoser-costura-overlock/ 
+
+          .row.texto-izquierda 
+            .col-md-5
+              .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+                h5 Figura 11.  
+                span Paso 4 
+              img(src='@/assets/curso/Tema2/fig11.png', alt='Mano sostiene un hilo azul que se retira cuidadosamente de una costura lateral, mientras otros hilos de distintos grosores permanecen sueltos sobre la tela.')
+              figcaption.text-regular Fuente: Fuente: https://www.naiicostura.com/3-maneras-de-descoser-costura-overlock/ 
+            .col-md-6.mb-4.mb-md-0
+              h4 Paso 4.  
+              h4 Romper las puntadas de la aguja.
+            
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Paso 5. 
+              h4 Por último, tirar la hebra y se terminará de desbaratar el filete.
+            .col-md-5
+              .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+                h5 Figura 12.  
+                span Paso 5
+              img(src='@/assets/curso/Tema2/fig12.png', alt='Mano sostiene un hilo azul que se retira cuidadosamente de una costura lateral, mientras otros hilos de distintos grosores permanecen sueltos sobre la tela.')
+              figcaption.text-regular Fuente: https://www.naiicostura.com/3-maneras-de-descoser-costura-overlock/ 
+
+    .bg7.p-3
+      .row.justify-content-center.align-items-center
+        .col-lg-4
+          p Una vez se han separado cuidadosamente las piezas necesarias para intervenir una prenda, dichas partes deben ser comprendidas y tratadas como lo que realmente representan: patrones activos. Es decir, moldes reales extraídos directamente de una prenda existente, susceptibles de ser modificados según criterios técnicos y necesidades específicas de ajuste.
+        .col-lg-2
+          img(src='@/assets/curso/Tema2/img03.svg', alt='')
+        .col-lg-4
+          p Las técnicas de arreglo que aquí se presentan se fundamentan en principios de adaptación morfológica, es decir, modificaciones basadas en las proporciones anatómicas de la persona usuaria, con el fin de mejorar la vestibilidad, funcionalidad y estética de la prenda.
 
 
+    Separador
+    #t_2_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 2.2 Enfoque técnico del proceso de modificación
 
-
-
-
-
-
-
-
-
-
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-9
+        p El punto de partida de cualquier intervención debe ser el análisis antropométrico, que permite identificar las diferencias entre las medidas corporales reales y las dimensiones que ofrece la prenda. A partir de esta lectura, se definen los puntos de ajuste necesarios, y se procede con la modificación directa sobre las piezas desarmadas o sobre los moldes derivados.
 
 
 
